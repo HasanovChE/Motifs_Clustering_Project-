b25032442cd35f18bb2a029aabf49ecc04c87c7e
Parvin Hasanov <hesenovpervin02@gmail.com>
2026-07-23 01:02:04 +0400

    Add sys path configuration for module imports

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
-
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # Import the orchestrator function from main.py
 from main import run_pipeline
 
